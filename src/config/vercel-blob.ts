e0bc360b135bccde3a7b530cafb83729f472dc5d
Lorenzo Fiori <lorenzofy@outlook.com>
2025-10-22 20:28:42 +0200

    update

--- src/config/vercel-blob.ts
+++ src/config/vercel-blob.ts
@@ -1,11 +1,10 @@
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 
 export const VercelBlob = vercelBlobStorage({
-    enabled: true,
-    clientUploads: true,
-    addRandomSuffix: true,
-    collections: {
-        media: true,
-    },
-    token: process.env.BLOB_READ_WRITE_TOKEN,
-})
+  enabled: true,
+  clientUploads: true,
+  collections: {
+    media: true,
+  },
+  token: process.env.BLOB_READ_WRITE_TOKEN,
+});
